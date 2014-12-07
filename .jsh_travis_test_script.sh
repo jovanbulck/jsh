234152a154adea734235e72382e5f2ee01daf58e
Jo Van Bulck <jo.vanbulck@student.kuleuven.be>
2014-12-07 23:50:55 +0100

    yet another test build

--- .jsh_travis_test_script.sh
+++ .jsh_travis_test_script.sh
@@ -1,7 +1,7 @@
 ## a file containing some test commands to be executed by jsh each time
 ## Travis CI triggers a new build
 
-echo -e "\\n----------- START OF JSH_TRAVIS_TEST_SCRIPT -----------\\n"
+echo -e "\\n\\033[1;36m----------- START OF JSH_TRAVIS_TEST_SCRIPT -----------\\033[0m\\n"
 
 echo Hi I am running jsh --version :
 echo "--------------------------"
@@ -17,6 +17,10 @@ echo "this is the output of \"ll | grep alias\"" && ll | grep alias
 
 echo -e "\\nthis is the output of \"ll | grep alias | wc\"" && ll | grep alias | wc
 
+echo -e "\\nnow triggering some error messages: "
+
 # a long comment: loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong
 
-echo -e "\\n----------- END OF JSH_TRAVIS_TEST_SCRIPT -----------\\n"
+some_none_existing_command
+
+echo -e "\\n\\033[1;36m----------- END OF JSH_TRAVIS_TEST_SCRIPT -----------\\033[0m\\n"
